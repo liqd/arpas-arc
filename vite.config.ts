41a77317671167764b0968126e4d0d1ba4c3f211
jKnepel <julian.knepel@mailbox.org>
2025-05-13 23:05:37 +0200

    chore: fix naming convention if config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/ARPAS_XR/',
+    base: '/arpas-arc/',
     plugins: [react()],
     server: {
         port: 3000,
