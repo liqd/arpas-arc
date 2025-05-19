f2686a3c3366b9516748691130788f33f568ddd4
Christoph Holtmann <christoph.holtmann@htw-berlin.de>
2025-05-19 12:02:56 +0200

    feat: add vite envs

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,5 +1,10 @@
 interface ImportMetaEnv {
     BASE_URL: string;
+    readonly VITE_MINIO_ENDPOINT: string;
+    readonly VITE_MINIO_REGION: string;
+    readonly VITE_MINIO_ACCESS_KEY: string;
+    readonly VITE_MINIO_SECRET_KEY: string;
+    readonly VITE_PUBLIC_NOAA_API_KEY: string;
 }
 
 interface ImportMeta {
